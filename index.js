a6883a55c720c3ab07d3a2d6a80a42b14c7f9257
nancy <nancy@192.168.20.118>
2022-04-27 09:25:42 +0800

    three things done

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 // button 按钮的效果
 const input = document.querySelector("input");
+const hide = document.getElementsByClassName("rating-board");
+const show = document.getElementsByClassName("thankyou-board");
 
-input.addEventListener("click", e => {
+input.addEventListener("click", (e) => {
   // location.href = "../html/thankyou.html";
   // TODO: psuedo code
   // 1. hide rating-board
   // 2. show thankyou-board
   // 3. change the text of thankyou-board to rating
+  // console.log(hide[0].classList);
+  // console.log(show[0].classList);
+  // console.log(typeof hide[0].classList);
+  hide[0].classList.add("switch-off");
+  show[0].classList.remove("switch-off");
 });
 
-input.addEventListener("mouseenter", e => {
+const result = document.querySelector(".result");
+input.addEventListener("click", (e) => {
+  result.innerText = `You selected ${rating} out of 5`;
+});
+
+input.addEventListener("mouseenter", (e) => {
   e.target.style.backgroundColor = "#E45826";
   e.target.style.cursor = "pointer";
+  console.log(this);
 });
 
-input.addEventListener("mouseout", e => {
+input.addEventListener("mouseout", (e) => {
   e.target.style.backgroundColor = "#f76e11";
 });
 
@@ -64,7 +77,7 @@ const stars = document.querySelectorAll(".rating .star");
 
 for (let index = 0; index < stars.length; index++) {
   const star = stars[index];
-  star.addEventListener("click", e => {
+  star.addEventListener("click", (e) => {
     rating = index + 1;
     console.log("rating: ", rating);
 
